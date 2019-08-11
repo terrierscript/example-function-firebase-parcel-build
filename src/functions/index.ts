911df66dc1704260f9e6754be98192477a600086
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-08-11 18:33:22 +0900

    add

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,8 +1,12 @@
-import functions from "firebase-functions"
+import * as functions from "firebase-functions"
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!")
+})
+exports.helloWorld2 = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!2")
+})
+
+exports.helloWorld3 = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!3")
 })
